4b6e5e967ac028c279e53ce00afaa0a66070eae8
Vincent Nys <vincentnys@gmail.com>
2016-09-16 08:39:43 +0200

    Switching machines.

--- src/tree.rkt
+++ src/tree.rkt
@@ -22,6 +22,7 @@
 
 #lang racket
 
+(require racket-list-utils/utils)
 (provide (struct-out node) nodeof)
 
 (struct node (label children)
@@ -42,3 +43,58 @@
   (λ (elem) (and (node? elem)
                  (type-predicate (node-label elem))
                  ((listof (nodeof type-predicate)) (node-children elem)))))
+
+(define (subtree-filter tree predicate)
+  (match tree
+    [(node label children)
+     (if (predicate tree)
+      (cons tree (append* (map (λ (c) subtree-filter c predicate) children)))
+      (append* (map (λ (c) subtree-filter c predicate) children)))]))
+; TODO provide, with a contract
+
+
+; TODO provide, with a contract
+(define (replace-first-subtree top replacee replacement)
+  (second (replace-first-subtree-aux top replacee replacement)))
+
+; result is a pair: whether replacement took place, plus the tree after replacement
+(define (replace-first-subtree-aux top replacee replacement)
+  (if (equal? top replacee)
+      (cons #t replacement)
+      (match top
+       [(node label children)
+        ; accumulator is whether-substitution-has-taken-place-yet
+        (mapAccum (λ (c acc)
+        ; dit gaat zo niet werken: alles behalve #f telt als #t, dus recursieve call zal altijd tellen als #t
+        ;(mapAccum (λ (c acc) (if acc (cons c acc) (replace-first-subtree-aux c replacee replacement))) #f children)
+
+; TODO provide, with a contract
+;
+
+
+
+
+
+
+;
+;-- | Replace the last instance of a subtree with a different subtree.
+;replaceLastSubtree :: (Eq a)
+;                   => Tree a -- ^ the top-level tree
+;                   -> Tree a -- ^ the original subtree
+;                   -> Tree a -- ^ the new subtree
+;                   -> Tree a -- ^ the resulting tree
+;replaceLastSubtree top sub1 sub2 = snd $ replaceLastSubtree' top sub1 sub2
+;
+
+      
+;-- | Replace the last instance of a subtree with a different subtree.
+;replaceLastSubtree' :: (Eq a)
+;                    => Tree a -- ^ the top-level tree
+;                    -> Tree a -- ^ the original subtree
+;                    -> Tree a -- ^ the new subtree
+;                    -> (Bool, Tree a) -- ^ whether replacement took place, plus the resulting tree
+;replaceLastSubtree' top sub1 sub2 
+;  | top == sub1 = (True, sub2)
+;replaceLastSubtree' (Node label children) sub1 sub2 = 
+;  (update, (Node label updatedChildren)) where
+;  (update, updatedChildren) = mapAccumR (\acc x -> if acc then (acc, x) else (replaceLastSubtree' x sub1 sub2)) False children
