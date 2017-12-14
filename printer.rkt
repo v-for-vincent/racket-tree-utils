eaee40fd93ee4f1e25446332443a695a6319ab1c
Vincent Nys <vincentnys@gmail.com>
2017-12-14 10:27:33 +0100

    This can go to the package server.

--- printer.rkt
+++ printer.rkt
@@ -23,43 +23,47 @@
 ; Note that the current code does not output a String (the expected functional behaviour).
 ; This is because a Void return type allows things like underlined terminal output.
 
-#lang racket
-(require unstable-positional-tree-utils)
-
-(define (list-init lst)
-  (take lst (- (length lst) 1)))
-
-(define (next-margin margin for-last-child?)
-  (cond [(and for-last-child? (equal? margin "")) "`-"]
-        [(equal? margin "") "+-"]
-        [(and for-last-child? (equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-")) (string-append (substring margin 0 (- (string-length margin) 2)) "| `-")]
-        [(equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-") (string-append (substring margin 0 (- (string-length margin) 2)) "| +-")]
-        [(and for-last-child? (equal? (substring margin (- (string-length margin) 2) (string-length margin)) "`-")) (string-append (substring margin 0 (- (string-length margin) 2)) "  `-")]
-        [else (string-append (substring margin 0 (- (string-length margin) 2)) "  +-")]))
-
-(define (display-padding margin out)
-  (cond [(equal? margin "") (displayln "|" out)]
-        [(equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-") (displayln (string-append (substring margin 0 (- (string-length margin) 2)) "| |") out)]
-        [else (displayln (string-append (substring margin 0 (- (string-length margin) 2)) "  |") out)]))
+#lang at-exp racket
+(require unstable-positional-tree-utils
+         scribble/srcdoc
+         (for-doc scribble/manual))
 
 (define (tree-display the-tree node-display [out (current-output-port)])
+  (define (tree-display-aux margin the-tree node-display out)
+    (define (next-margin margin for-last-child?)
+      (cond [(and for-last-child? (equal? margin "")) "`-"]
+            [(equal? margin "") "+-"]
+            [(and for-last-child? (equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-")) (string-append (substring margin 0 (- (string-length margin) 2)) "| `-")]
+            [(equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-") (string-append (substring margin 0 (- (string-length margin) 2)) "| +-")]
+            [(and for-last-child? (equal? (substring margin (- (string-length margin) 2) (string-length margin)) "`-")) (string-append (substring margin 0 (- (string-length margin) 2)) "  `-")]
+            [else (string-append (substring margin 0 (- (string-length margin) 2)) "  +-")]))
+    (define (display-padding margin out)
+      (cond [(equal? margin "") (displayln "|" out)]
+            [(equal? (substring margin (- (string-length margin) 2) (string-length margin)) "+-") (displayln (string-append (substring margin 0 (- (string-length margin) 2)) "| |") out)]
+            [else (displayln (string-append (substring margin 0 (- (string-length margin) 2)) "  |") out)]))  
+    (if (null? (node-children the-tree))
+        (begin (display margin out) (node-display the-tree out) (newline out))
+        (begin (display margin out)
+               (node-display the-tree out)
+               (newline out)
+               (map (λ (c) (begin (display-padding margin out) (tree-display-aux (next-margin margin #f) c node-display out))) (drop-right (node-children the-tree) 1))
+               (display-padding margin out)
+               (tree-display-aux (next-margin margin #t) (last (node-children the-tree)) node-display out))))
   (tree-display-aux "" the-tree node-display out))
-
-(define (tree-display-aux margin the-tree node-display out)
-  (if (null? (node-children the-tree))
-      (begin (display margin out) (node-display the-tree out) (newline out))
-      (begin (display margin out)
-             (node-display the-tree out)
-             (newline out)
-             (map (λ (c) (begin (display-padding margin out) (tree-display-aux (next-margin margin #f) c node-display out))) (list-init (node-children the-tree)))
-             (display-padding margin out)
-             (tree-display-aux (next-margin margin #t) (last (node-children the-tree)) node-display out))))
 
 (module+ test
   (require rackunit)
-  (define tree-drawing
-    (substring
-     "
+  (let* ([g-tree (node "g" '())]
+         [f-tree (node "f" '())]
+         [e-tree (node "e" '())]
+         [d-tree (node "d" '())]
+         [c-tree (node "c" (list f-tree g-tree))]
+         [b-tree (node "b" (list e-tree))]
+         [a-tree (node "a" (list b-tree c-tree d-tree))]
+         [string-port (open-output-string)]
+         [expected
+          (substring
+           "
 a
 |
 +-b
@@ -73,20 +77,13 @@ a
 | `-g
 |
 `-d
-" 1))
+" 1)]
+         [actual (begin (tree-display a-tree (λ (t o) (display (node-label t) o)) string-port) (get-output-string string-port))])
+    (check-equal? actual expected)))
 
-  (define (node-display tree out)
-    (display (node-label tree) out))
-
-  (let* ([g-tree (node "g" '())]
-         [f-tree (node "f" '())]
-         [e-tree (node "e" '())]
-         [d-tree (node "d" '())]
-         [c-tree (node "c" (list f-tree g-tree))]
-         [b-tree (node "b" (list e-tree))]
-         [a-tree (node "a" (list b-tree c-tree d-tree))]
-         [string-port (open-output-string)]
-         [actual (begin (tree-display a-tree node-display string-port) (get-output-string string-port))])
-    (check-equal? actual tree-drawing)))
-
-(provide tree-display)
+(provide
+ (proc-doc/names
+  tree-display
+  (->* (node? procedure?) (output-port?) void?)
+  ((n proc) ((o (current-output-port))))
+  @{Pretty-prints @racket[t] to @racket[o], using @racket[proc] to compute the string representation of a single @racket[node?] (which is usually only a visualization of the label).}))
---
+++ scribblings/unstable-positional-tree-utils.scrbl
@@ -0,0 +1,9 @@
+#lang scribble/manual
+@(require
+   scribble/extract)
+@title{Unstable positional tree utilities}
+This is a simple collection of tree-processing functions which I have found helpful in general.
+@author{Vincent Nys}
+@defmodule[unstable-positional-tree-utils]
+@section{API}
+@include-extracted[unstable-positional-tree-utils]
